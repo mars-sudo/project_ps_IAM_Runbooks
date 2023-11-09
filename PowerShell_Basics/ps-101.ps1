fa52c1527f6f0443e9dd44dadc8bddf8f1cb8178
mars-sudo <robert.martin92@protonmail.com>
2023-11-08 20:30:23 -0500

    added ps-102 file

--- PowerShell_Basics/ps-101.ps1
+++ PowerShell_Basics/ps-101.ps1
@@ -56,7 +56,9 @@ help Get-Command -Full | Out-GridView
 # Let's create a Variable Object named $string, then see the Properties and Methods associated with Object
 $string = Write-Output 'Hello World'
 $string
-$string | Get-Member # This will show you the 'Type' of object is 
+$string | Get-Member # This will show you the 'TypeName: ' of object is String
+
+##########################################
 
 # To reference any of the Get-Member Properties, use dot notation
 $string.Length
@@ -66,17 +68,4 @@ $string.ToUpper() # Empty parentheses means that thte method has no arguments
 $string.ToLower()
 $string.Replace('Hello','Goodbye')
 
-# Building a powershell Hast Table - a group of key value pairs
-$hashtable = @{Color = 'Red';Transmission ='Automatic'; Convertable =$false}
 
-$hashtable | Get-Member # Here we will see the 'Type' of object is a System.Collections.Hashtable
-
-[pscustomobject]$hashtable # This creates custom properties for the object. 
-
-$car = [PSCustomObejct]$hashtable
-$car | Get-Memeber # Here we will see the 'TypeName' of this object is now a PSCustomObject with the 3 properties created in the Hash Table
-
-
-Get-Content -Path 'C:\Users\~' | ForEach-Object {[pscustomobject]@{ComputerName =$PSItem}} | Test-Connection
-
-###############################
